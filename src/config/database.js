32eef6085a4892602febc99f4f7db8311d9f99af
Juan Petterson Heberle <juan.petterson@gmail.com>
2019-02-09 23:42:30 -0200

    Commiting GoBarber v1

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: 'cjL6qZfo',
+  database: 'gobarber',
+  operatorAliases: false,
+  define: {
+    timestampts: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
